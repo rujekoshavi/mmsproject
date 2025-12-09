815f13d5684df7bd8a92437c79e05559b93a11f4
rujekoshavi <rujekoshavi@gmail.com>
2025-12-08 23:15:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -128,4 +128,5 @@ if __name__ == "__main__":
     if not os.path.exists(DB_PATH):
         print("WARNING: ProjectDB.db not found.")
         print("Run: python3 init_db.py")
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=False, host='0.0.0.0', port=port)
